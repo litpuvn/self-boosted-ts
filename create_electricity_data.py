4b9b8caaba1df3cf75203f847b5ab56359dcb0f4
Hoang Long <litpuvn@gmail.com>
2019-08-19 14:14:10 -0500

    add data cleaning, averaging content

---
+++ create_electricity_data.py
@@ -0,0 +1,32 @@
+import csv
+import numpy as np
+
+
+# path = '/home/long/Desktop/DDL/projects/self-boosted-timeseries/datasets/LD2011-2014/test.csv'
+path = '/home/long/Desktop/DDL/projects/self-boosted-timeseries/datasets/LD2011-2014/LD2011_2014.txt'
+
+
+remove_comma = lambda t: t.replace(',', '.')
+
+
+with open(path) as file_pointer:
+    reader = csv.reader(file_pointer, delimiter=';')
+
+    with open('data/clean_electricity.csv', 'w', newline='') as file_writer:
+        writer = csv.writer(file_writer)
+        for idx, row in enumerate(reader):
+            if idx < 1:
+                writer.writerow(['time, avg_electricity'])
+                continue
+
+            my_date = row[0]
+            removed_commas = np.array([remove_comma(xi) for xi in row[1:]])
+
+            numerical_data = np.asarray(removed_commas, dtype=np.float32)
+            non_zero = numerical_data[numerical_data != 0]
+            if len(non_zero) < 1:
+                print('dropping row:', idx)
+                continue
+            avg = np.sum(numerical_data, axis=0) / len(numerical_data)
+            writer.writerow([my_date, avg / 4])
+            print("datetime:", my_date, "average:", avg)
