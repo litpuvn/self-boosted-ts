8b9a855f27119c3e300de52675c41fdb3b624908
Hoang Long <litpuvn@gmail.com>
2019-08-22 13:25:03 -0500

    update

--- learning-examples/gp_keras.py
+++ learning-examples/gp_keras.py
@@ -6,8 +6,6 @@ from kgp.models import Model
 from kgp.losses import gen_gp_loss
 
 
-
-
 input_shape = (10, 2)  # 10 time steps, 2 dimensions
 batch_size = 32
 nb_train_samples = 512
@@ -16,9 +14,7 @@ gp_hypers = {'lik': -2.0, 'cov': [[-0.7], [0.0]]}
 # Build the model
 inputs = Input(shape=input_shape)
 rnn = SimpleRNN(32)(inputs)
-gp = GP(gp_hypers,
-        batch_size=batch_size,
-        nb_train_samples=nb_train_samples)
+gp = GP(gp_hypers, batch_size=batch_size, nb_train_samples=nb_train_samples)
 outputs = [gp(rnn)]
 model = Model(inputs=inputs, outputs=outputs)
 
@@ -26,3 +22,4 @@ model = Model(inputs=inputs, outputs=outputs)
 loss = [gen_gp_loss(gp) for gp in model.output_layers]
 model.compile(optimizer=Adam(1e-2), loss=loss)
 
+model.summary()
