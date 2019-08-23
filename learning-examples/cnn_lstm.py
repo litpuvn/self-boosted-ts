3a6d5f1d85531cf86e8531a85457a3ba3a5335cd
Hoang Long <litpuvn@gmail.com>
2019-08-23 16:34:34 -0500

    add cnn lstm

--- learning-examples/cnn_lstm.py
+++ learning-examples/cnn_lstm.py
@@ -1,22 +1,20 @@
+from keras.engine.topology import Input
 from keras.layers.convolutional import Conv1D
 from keras.layers.core import Flatten, Dense
 from keras.layers.pooling import MaxPooling1D
+from keras.layers.recurrent import LSTM
 from keras.layers.wrappers import TimeDistributed
 from keras.models import Sequential
-from torch.nn.modules.rnn import LSTM
 
 model = Sequential()
-
-conv = Conv1D(filter=5, kernel_size=3, activation='relu', batch_input_shape=(24, None, 1))
-model.add(TimeDistributed(conv))
-
-maxpool = MaxPooling1D(pool_size=2)
-model.add(TimeDistributed(maxpool))
+model.add(TimeDistributed(Conv1D(filters=5, kernel_size=3, activation='relu'), batch_input_shape=(24, None, 24, 1)))
+model.add(TimeDistributed(MaxPooling1D(pool_size=2)))
 model.add(TimeDistributed(Flatten()))
-model.add(LSTM(50, stateful=True, return_sequence=True))
-model.add(LSTM(10, stateful=True, return_sequence=True))
+model.add(LSTM(50, stateful=True, return_sequences=True))
+model.add(LSTM(10, stateful=True, return_sequences=True))
 model.add(Dense(24))
 
+model.summary()
 
 model.compile(optimizer='adam', loss='mse', metrics=['mae', 'mape', 'acc'])
 
