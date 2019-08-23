8bd8fe877b744bc90a4a687bfc0b76ce26fca63c
Hoang Long <litpuvn@gmail.com>
2019-08-23 15:31:57 -0500

    add printing summary of the model

--- learning-examples/multivariate_lstm.py
+++ learning-examples/multivariate_lstm.py
@@ -98,6 +98,8 @@ model = Sequential()
 model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
+
+model.summary()
 # fit network
 history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
 # plot history
