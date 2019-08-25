cd3ba7969b67dc95121b6d44a4e37d0959d3faf5
Opeyemi Openiyi <44436984+doubleope@users.noreply.github.com>
2019-08-25 15:30:10 -0500

    add rmse

--- electricity/RNN_enc_dec.py
+++ electricity/RNN_enc_dec.py
@@ -380,3 +380,8 @@ plt.xlabel('timestamp', fontsize=12)
 plt.ylabel('avg_electricity', fontsize=12)
 plt.show()
 
+from sklearn.metrics import mean_squared_error
+from math import sqrt
+
+rmse = sqrt(mean_squared_error(eval_df['actual'], eval_df['prediction']))
+print(rmse)
