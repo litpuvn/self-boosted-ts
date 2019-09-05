119626ce8d21f64616fe0c5bad5d5a4eaa495e15
Hoang Long <litpuvn@gmail.com>
2019-09-05 17:11:23 -0500

    add ploting rmse

--- plot_rmse_bar_charts.py
+++ plot_rmse_bar_charts.py
@@ -11,7 +11,7 @@ rmse_exchange_rate = [0.0130, 0.0097, 0.0106, 0.0153, 0.0082]
 def do_sub_plot(subplot, rmse_data, width=0.2):
     for i in range(len(rmse_data)):
         rmse_v = rmse_data[i]
-        subplot.bar(1 + i * width, rmse_v, width,
+        subplot.bar(0.2 + i * width, rmse_v, width,
                 alpha=0.5,
                 # color='w',
                 # hatch=patterns[6],
@@ -26,19 +26,22 @@ do_sub_plot(subplot=ax1, rmse_data=rmse_elec, width=width)
 do_sub_plot(subplot=ax2, rmse_data=rmse_temperature, width=width)
 do_sub_plot(subplot=ax3, rmse_data=rmse_exchange_rate, width=width)
 
-ax1.set_xlim(0, 3)
+# ax1.set_xlim(0, 3)
 ax1.set_ylim(80, 270)
 # ax1.set_yticks([80, 150, 220])
 
-ax2.set_xlim(0, 3)
-ax3.set_xlim(0, 3)
+# ax2.set_xlim(0, 3)
+# ax3.set_xlim(0, 3)
 
 ax1.set_xticks([])
 ax2.set_xticks([])
 ax3.set_xticks([])
 
 # ax3.set(xlabel='x-label', ylabel='y-label')
-fig.set_figheight(8)
+fig.set_figheight(22)
+fig.set_figwidth(12)
+
+ax1.legend(['MTL', 'MTV', 'Proposed', "Self-boosted", "proposed2"], loc='upper right', ncol=5)
 
 plt.savefig("results/avg_rmse_comparison.png")
 plt.show()
